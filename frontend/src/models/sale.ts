38964ccc6f9fadf966da56efede099963b352000
rrozdoce <felipefelipevilhalva@gmail.comm>
2022-11-18 19:05:22 -0400

    Sale Listing

---
+++ frontend/src/models/sale.ts
@@ -0,0 +1,13 @@
+/*
+{id: 37, sellerName: 'Padme', visited: 82, deals: 82, amount: 22465, …}
+*/
+
+export type Sale = {
+    id: number,
+    sellerName: String,
+    date: string,
+    visited: number,
+    deals: number, 
+    amount: number
+}
+
